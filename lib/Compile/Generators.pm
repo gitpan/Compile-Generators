1cfc080fbb27f32df456d292e7f7c239270af968
Ingy döt Net <ingy@cpan.org>
2006-07-03 22:12:45 +0000

    Import of INGY/Compile-Generators-0.11 from CPAN.
    gitpan-cpan-distribution: Compile-Generators
    gitpan-cpan-version:      0.11
    gitpan-cpan-path:         INGY/Compile-Generators-0.11.tar.gz
    gitpan-cpan-author:       INGY
    gitpan-cpan-maturity:     released

--- lib/Compile/Generators.pm
+++ lib/Compile/Generators.pm
@@ -2,11 +2,11 @@ package Compile::Generators;
 use strict;
 use 5.006001;
 use warnings;
-our $VERSION = '0.10';
+our $VERSION = '0.11';
 
 use Module::Compile -base;
 
-my $label = 'LABEL_AAAA';
+my $label = 'GENAAAA';
 
 sub pmc_compile {
     my ($class, $input) = @_;
@@ -29,21 +29,24 @@ sub compile {
         $dynamic = $static;
         $static = '';
     }
-    while ($dynamic =~ s/^(\s*yield\s*(.*))/#$1\n;;;;;;; \$__GEN_STATE__ = '$label'; return $2; ${label}:/m) {
+    while ($dynamic =~ s/^(\s*yield\s*(.*))/#$1\n        \$__GEN_STATE__ = '$label'; return $2; ${label}:/m) {
         $label++;
     }
 #     $dynamic =~ s/^/    /mg;
     
     my $code = <<"_";
-${start};;; my \$__GEN_STATE__ = undef;
+${start}    my \$__GEN_STATE__ = undef;
 ${static}
-;;; return sub {
-;;;;;;; goto \$__GEN_STATE__ if defined \$__GEN_STATE__;
+return sub {
+    goto \$__GEN_STATE__ if defined \$__GEN_STATE__;
 ${dynamic}
-;;; return;
-;;; }
+    \$__GEN_STATE__ = '$label';
+    ${label}:
+    return;
+}
 ${end}
 _
+    $label++;
     $source =~ s/^sub .*:generator .*\n(?s:.*?)^\}.*\n/$code/m;
     return $source;
 }
@@ -54,7 +57,7 @@ __DATA__
 
 =head1 NAME
 
-Compile::Generators - Write Python-like generators in Perl
+Compile::Generators - Python-like generator subroutines for Perl
 
 =head1 SYNOPSIS
 
@@ -62,9 +65,9 @@ Compile::Generators - Write Python-like generators in Perl
 
     sub gen_range :generator {
         my ($min, $max) = @_;
+        my $num = $min;
         my $incr;
 
-        my $num = $min;
         while (not defined $max or $num < $max) {
             $incr = shift || 1;
             yield $num;
@@ -73,27 +76,40 @@ Compile::Generators - Write Python-like generators in Perl
     }
 
     my $range = gen_range(50, 100);
-    my $incr = gen_range(1);
+    my $i = gen_range(1);
 
-    while (my $num = $range->($incr->())) {
+    while (my $num = $range->($i->())) {
         print "\$num => $num\n";
     }
+
+When run, this prints:
+
+    $num => 50
+    $num => 51
+    $num => 53
+    $num => 56
+    $num => 60
+    $num => 65
+    $num => 71
+    $num => 78
+    $num => 86
+    $num => 95
 
 =head1 DESCRIPTION
 
 Compile::Generators lets you define subroutines that return their code
 as a generator. You can then call the generator over and over until it
-returns undef. The generator can yield (return) a value from and then
+returns an empty list. The generator can yield (return) a value and then
 when you call it again it resumes right after the yield.
 
 =head1 USAGE
 
-Any subroutine marked with the a C<:generator> attribute will have it's
+Any subroutine marked with the a C<:generator> attribute will have its
 code wrapped into a closure and returned by the subroutine. Any yield
-statments will be replace with code to return/resume at that point.
+statements will be replace with code to return/resume at that point.
 
 Any code before the first blank line in the sub will B<not> be a part of
-the closure but will be exectuted when the sub is actually called. This
+the closure but will be executed when the sub is actually called. This
 means that any variables that are defined before the blank line will be
 I<closed> by the generator sub.
 
@@ -103,6 +119,11 @@ the C<.pmc> to see what is really happening.
 Since this module uses C<goto> statements, you cannot C<yield> inside a
 C<for> loop. Perl does not allow this. However you I<can> use C<while>
 statements.
+
+Currently a C<yield> statement needs to be a simple statement on it's own
+line, since this gets parsed and replaced by a return/goto-label.
+
+See the tests for examples.
 
 =head1 AUTHOR
 
--- t/generator.t
+++ t/generator.t
@@ -1,10 +1,7 @@
 use Compile::Generators;
+use strict;
 
 use Test::More tests => 3;
-
-sub bogus1 {
-    return 42;
-}
 
 sub gen_range :generator {
     my ($min, $max) = @_;
@@ -18,6 +15,8 @@ sub gen_range :generator {
     }
 }
 
+# Test calling two generators from the same sub at the same time.
+# Yay!
 my $range = gen_range(50, 100);
 my $incr = gen_range(1);
 
@@ -26,12 +25,9 @@ while (my $num = $range->($incr->())) {
     push @numbers, $num;
 }
 
-sub bogus2 {
-    return 42;
-}
-
 is(join('-', @numbers), '50-51-53-56-60-65-71-78-86-95', "generator works");
 
 ok ((-f 't/generator.tc'), "Compiled file exists");
+
 pass __FILE__;
 
